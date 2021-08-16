00eecf17d29515b80831ef313d764eaf6de59c5c
MaxMuecket <max.muecket@googlemail.com>
2021-08-16 11:42:39 +0200

    :sparkles: Add SearchButton component

---
+++ src/app/components/SearchButton/SearchButton.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import SearchButton from './SearchButton';
+
+export default {
+  title: 'Component/SearchButton',
+  component: SearchButton,
+};
+
+export const Search = (): JSX.Element => <SearchButton>Search</SearchButton>;
